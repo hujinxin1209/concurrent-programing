903f31896e5c0605d5d22d6c5002c9a571c99ef4
hujinxin1209 <565831772@qq.com>
2018-08-24 23:06:09 +0800

    ScheduleExecutorServiceDemo

---
+++ src/chapter03/ScheduleExecutorServiceDemo.java
@@ -0,0 +1,22 @@
+package chapter03;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import java.util.concurrent.TimeUnit;
+
+public class ScheduleExecutorServiceDemo {
+	public static void main(String[] args) {
+		ScheduledExecutorService ses = Executors.newScheduledThreadPool(10);
+		ses.scheduleAtFixedRate(new Runnable() {
+			public void run() {
+				try {
+					Thread.sleep(1000);
+					System.out.println(System.currentTimeMillis() / 1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}, 0, 2, TimeUnit.SECONDS);
+	}
+}
